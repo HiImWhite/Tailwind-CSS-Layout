cad8bafccfc3076485800d2e66fe71700d97d99e
HiImWhite <wojttasek1999@gmail.com>
2022-08-13 17:45:20 +0200

    Importing files

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {  backgroundImage: {
+      'split-white-gray': "linear-gradient(to bottom, #EBEBEB 80% , white 20%);"
+    }  },
+  },
+  plugins: [],
+}
